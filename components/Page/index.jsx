a870bd0438cd19a43dba565b3cca39058d36d845
komaschuk <d.komaschuk@gmail.com>
2017-02-06 15:57:47 +0200

    add svg to Page

--- components/Page/index.jsx
+++ components/Page/index.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { prefixLink } from 'gatsby-helpers'
 
 import './style.scss';
+import BbqLogo from '../../static/img/svg-icons/bbq-agency-logo.svg'
 
 class Page extends React.Component {
     render() {
@@ -11,13 +12,18 @@ class Page extends React.Component {
         return (
             <div className='page'>
                 <div className='page__content'>
+
+                    <img src={ (BbqLogo) } className='page__img' alt='BBQ Agency Logo SVG' />
+
                     <h1>BBQ Agency</h1>
+
                     <div className='page__text'>
                         <span>Ukraine, Kyiv</span>
                         <span>+ 380 66 665 65 65</span>
                         <span>+ 380 66 665 65 65</span>
                         <span>info@bbq.com</span>
                     </div>
+
                 </div>
             </div>
         );
