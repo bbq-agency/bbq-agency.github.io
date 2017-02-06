fbc2d933265c7ffd2b31a3737205ae9abb730d41
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2017-02-06 16:11:52 +0200

    add more svg into HomePage

--- components/HomePage/index.jsx
+++ components/HomePage/index.jsx
@@ -4,12 +4,21 @@ import { prefixLink } from 'gatsby-helpers'
 import { config } from 'config'
 
 import { MorphReplace, MorphReplaceResize } from 'react-svg-morph';
+
 import LogoCore from '../SVG/LogoCore';
 import LogoDesign from '../SVG/LogoDesign';
 import LogoDevops from '../SVG/LogoDevops';
 import LogoFront from '../SVG/LogoFront';
 import LogoInegration from '../SVG/LogoInegration';
 import LogoMarketing from '../SVG/LogoMarketing';
+import LogoOperation from '../SVG/LogoOperation';
+import LogoPm from '../SVG/LogoPm';
+import LogoProduct from '../SVG/LogoProduct';
+import LogoPromo from '../SVG/LogoPromo';
+import LogoQa from '../SVG/LogoQa';
+import LogoRamon from '../SVG/LogoRamon';
+import LogoSmm from '../SVG/LogoSmm';
+import LogoTimur from '../SVG/LogoTimur';
 
 import './style.scss'
 
@@ -31,18 +40,34 @@ class HomePage extends React.Component {
 
         let icon
 
-        if (this.increment % 6 === 1) {
+        if (this.increment % 14 === 1) {
             icon = <LogoCore key='LogoCore' />
-        } else if (this.increment % 6 === 2) {
+        } else if (this.increment % 14 === 2) {
             icon = <LogoDesign key='LogoDesign' />
-        } else if (this.increment % 6 === 3) {
+        } else if (this.increment % 14 === 3) {
             icon = <LogoDevops key='LogoDevops' />
-        } else if (this.increment % 6 === 4) {
+        } else if (this.increment % 14 === 4) {
             icon = <LogoFront key='LogoFront' />
-        } else if (this.increment % 6 === 5) {
+        } else if (this.increment % 14 === 5) {
             icon = <LogoInegration key='LogoInegration' />
-        } else {
+        } else if (this.increment % 14 === 6) {
             icon = <LogoMarketing key='LogoMarketing' />
+        } else if (this.increment % 14 === 7) {
+            icon = <LogoOperation key='LogoOperation' />
+        } else if (this.increment % 14 === 8) {
+            icon = <LogoPm key='LogoPm' />
+        } else if (this.increment % 14 === 9) {
+            icon = <LogoProduct key='LogoProduct' />
+        } else if (this.increment % 14 === 10) {
+            icon = <LogoPromo key='LogoPromo' />
+        } else if (this.increment % 14 === 11) {
+            icon = <LogoQa key='LogoQa' />
+        } else if (this.increment % 14 === 12) {
+            icon = <LogoRamon key='LogoRamon' />
+        } else if (this.increment % 14 === 13) {
+            icon = <LogoSmm key='LogoSmm' />
+        } else {
+            icon = <LogoTimur key='LogoTimur' />
         }
 
         return (
