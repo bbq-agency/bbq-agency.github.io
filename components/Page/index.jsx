d692cc10ec4e735ea780c2f645f07db12f5b812e
komaschuk <d.komaschuk@gmail.com>
2017-02-06 15:17:50 +0200

    update Page

--- components/Page/index.jsx
+++ components/Page/index.jsx
@@ -10,12 +10,14 @@ class Page extends React.Component {
 
         return (
             <div className='page'>
-                <h1>{ page.title }</h1>
-                <div className='page__text'>
-                    <span>Ukraine, Kyiv</span>
-                    <span>+ 380 66 665 65 65</span>
-                    <span>+ 380 66 665 65 65</span>
-                    <span>info@bbq.com</span>
+                <div className='page__content'>
+                    <h1>BBQ Agency</h1>
+                    <div className='page__text'>
+                        <span>Ukraine, Kyiv</span>
+                        <span>+ 380 66 665 65 65</span>
+                        <span>+ 380 66 665 65 65</span>
+                        <span>info@bbq.com</span>
+                    </div>
                 </div>
             </div>
         );
