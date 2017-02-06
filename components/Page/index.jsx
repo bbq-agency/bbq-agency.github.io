00207cd63143abad64b140a1b6326f5aa489e33b
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2017-02-06 17:45:53 +0200

    upd Page

--- components/Page/index.jsx
+++ components/Page/index.jsx
@@ -15,12 +15,7 @@ class Page extends React.Component {
 
                     <img src={ (BbqLogo) } className='page__img' alt='BBQ Agency Logo SVG' />
 
-                    {/*<h1>BBQ Agency</h1>*/}
-
                     <div className='page__text'>
-                        <span>Ukraine, Kyiv</span>
-                        <span>+ 380 66 665 65 65</span>
-                        <span>+ 380 66 665 65 65</span>
                         <span>info@bbq.com</span>
                     </div>
 
