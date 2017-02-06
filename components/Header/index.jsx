9636c462250fbff6e62248a88d7833e52cc78a5c
komaschuk <d.komaschuk@gmail.com>
2017-02-06 13:32:01 +0200

    update Header

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -13,7 +13,7 @@ class Header extends React.Component {
             <div className='header'>
 
                 <Link to={ prefixLink('/')} className='header__link header__link--logo' activeClassName='is-selected' onlyActiveOnIndex>
-                    <img src={ prefixLink(BbqLogo) } alt='BBQ Agency Logo SVG' />
+                    {/*<img src={ prefixLink(BbqLogo) } alt='BBQ Agency Logo SVG' />*/}
                     <span>BBQ Agency</span>
                     <Ink />
                 </Link>
